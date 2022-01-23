13868c33a267b1f1d36ca25b6623e5001bd0da46
Htc Ganesh <Htc Ganesh@HtcGanesh-PC>
2022-01-23 11:00:33 +0530

    First

---
+++ Jenkins2/src/test/java/jenkins2/test2.java
@@ -0,0 +1,28 @@
+package jenkins2;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class test2 {
+
+	@Test
+	public void chrome () {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\driver\\chromedriver.exe");
+		WebDriver d = new ChromeDriver();
+		d.get("http://demo.automationtesting.in/Alerts.html");
+		d.manage().window().maximize();
+		WebElement FirstAlert = d.findElement(By.xpath("//button[@class='btn btn-danger']"));
+		//FirstAlert.click();
+		Alert Alert1 = d.switchTo().alert();
+		System.out.println(Alert1.getText());
+		Alert1.accept();
+		
+		
+	}
+}
